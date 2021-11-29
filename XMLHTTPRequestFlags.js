5e061546d39628edea3e662089f08d63f0647a97
pandimech <95157823+pandimech@users.noreply.github.com>
2021-11-29 10:42:55 +0530

    Update XMLHTTPRequestFlags.js

--- XMLHTTPRequestFlags.js
+++ XMLHTTPRequestFlags.js
@@ -1,5 +1,5 @@
 var request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.com/v2/all', true);
+request.open('GET', 'https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json', true);
 request.send();
 request.onload = function() {
     var data = JSON.parse(this.response);
